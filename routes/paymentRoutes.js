a463d5933a737647f476c352581973ea0dbe342c
Nguyen Quoc Khanh <khenng208@gmail.com>
2025-11-20 00:09:00 +0700

    update

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -10,7 +10,7 @@ const {
 const { protect, authorize } = require('../middleware/authMiddleware'); // Import
 
 // Route để xem lịch sử (Chỉ Admin)
-router.get('/', protect, authorize('admin'), paymentController.getAllPayments);
+router.get('/', protect, authorize('admin'), getAllPayments);
 
 // --- SỬA Ở ĐÂY ---
 // Route để tạo yêu cầu thanh toán (Chỉ Customer)
@@ -19,6 +19,6 @@ router.post('/', protect, authorize('customer'), createPayment);
 // --- KẾT THÚC SỬA ---
 
 // Route để Momo gọi về (IPN)
-router.post('/momo-ipn', paymentController.handleMomoIPN);
+router.post('/momo-ipn', handleMomoIPN);
 
 module.exports = router;
