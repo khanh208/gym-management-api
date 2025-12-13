afef4816a352ae31bb31970dfc5c94c375897dc2
Nguyen Quoc Khanh <khenng208@gmail.com>
2025-12-13 19:08:57 +0700

    update

--- routes/checkInRoutes.js
+++ routes/checkInRoutes.js
@@ -1,11 +1,14 @@
 // routes/checkInRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getCustomerCheckInInfo } = require('../controllers/checkInController');
+// SỬA: Import thêm createWalkInTicket vào danh sách destructuring
+const { getCustomerCheckInInfo, createWalkInTicket } = require('../controllers/checkInController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Chỉ Admin và Trainer mới được quét check-in
 router.get('/customer/:khach_id', protect, authorize('admin', 'trainer'), getCustomerCheckInInfo);
-router.post('/walk-in', checkInController.createWalkInTicket);
+
+// SỬA: Gọi trực tiếp hàm createWalkInTicket (thay vì checkInController.createWalkInTicket)
+router.post('/walk-in', createWalkInTicket);
 
 module.exports = router;
